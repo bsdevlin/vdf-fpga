ec56aac0172f601847f097210a2ad81de783b942
bsdevlin <bsdevlin@LAPTOP-AL12KDU7>
2019-08-30 23:38:43 +0800

    Added wrapper for sq module and renamed testbench

---
+++ poly_mod_mult/rtl/poly_mod_sq_wrapper.sv
@@ -0,0 +1,70 @@
+/*
+  In this wrapper we check the result is not greater than MODULUS and
+  do a correction subtraction if it is, and then convert to integer form.
+
+  Copyright (C) 2019  Benjamin Devlin.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+module poly_mod_sq_wrapper #(
+  parameter int                       WORD_BITS = 8,
+  parameter int                       NUM_WORDS = 4,
+  parameter [WORD_BITS*NUM_WORDS-1:0] MODULUS = 128,
+  parameter int                       REDUCTION_BITS = 9,
+  parameter int                       REDUN_WORD_BITS = 1,
+  parameter int                       I_WORD = NUM_WORDS + 1,
+  parameter int                       COEF_BITS = WORD_BITS + REDUN_WORD_BITS
+) (
+  input i_clk,
+  input i_rst,
+  input i_val,
+  input [I_WORD-1:0][COEF_BITS-1:0]        i_dat,
+  output logic [I_WORD-1:0][COEF_BITS-1:0] o_dat,
+  output logic                             o_val
+);
+
+// For now just add pipeline stages to the input and output
+localparam PIPES = 3;
+
+logic [PIPES-1:0]                            val_in, val_out;
+logic [PIPES-1:0][I_WORD-1:0][COEF_BITS-1:0] dat_in, dat_out;
+
+always_ff @ (posedge i_clk) begin
+  val_in <= {val_in, i_val};
+  dat_in <= {dat_in, i_dat};
+  {o_val, val_out[PIPES-1:1]} <= val_out;
+  {o_dat, dat_out[PIPES-1:1]} <= dat_out;
+end
+
+poly_mod_mult #(
+  .SQ_MODE           ( 1'd1            ),
+  .WORD_BITS         ( WORD_BITS       ),
+  .NUM_WORDS         ( NUM_WORDS       ),
+  .MODULUS           ( MODULUS         ),
+  .REDUCTION_BITS    ( REDUCTION_BITS  ),
+  .REDUN_WORD_BITS   ( REDUN_WORD_BITS ),
+  .I_WORD            ( I_WORD          ),
+  .COEF_BITS         ( COEF_BITS       )
+)
+poly_mod_mult_i (
+  .i_clk   ( i_clk ),
+  .i_rst   ( i_rst ),
+  .i_val   ( val_in[PIPES-1] ),
+  .i_dat_a ( dat_in[PIPES-1] ),
+  .i_dat_b ( '0              ),
+  .o_dat   ( dat_out[0]      ),
+  .o_val   ( val_out[0]      )
+);
+
+endmodule
--- poly_mod_mult/tb/poly_mod_sq_tb.sv
+++ poly_mod_mult/tb/poly_mod_sq_tb.sv
@@ -16,7 +16,7 @@
 */
 `timescale 1ps/1ps
 
-module poly_mod_mult_tb ();
+module poly_mod_sq_tb ();
 
 localparam CLK_PERIOD = 100;
 localparam WORD_BITS = 16;
@@ -29,7 +29,7 @@ localparam [WORD_BITS*NUM_WORDS-1:0] MODULUS = (1 << 127) - 10;
 
 logic clk, rst;
 
-logic [I_WORD-1:0][COEF_BITS-1:0] i_dat_a, i_dat_b;
+logic [I_WORD-1:0][COEF_BITS-1:0] i_dat_a;
 logic [I_WORD*COEF_BITS-1:0] in_a;
 logic [I_WORD-1:0][COEF_BITS-1:0] o_dat;
 logic o_val, start;
@@ -44,8 +44,7 @@ initial begin
   forever #CLK_PERIOD clk = ~clk;
 end
 
-poly_mod_mult #(
-  .SQ_MODE         ( 1               ),
+poly_mod_sq_wrapper #(
   .WORD_BITS       ( WORD_BITS       ),
   .NUM_WORDS       ( NUM_WORDS       ),
   .REDUN_WORD_BITS ( REDUN_WORD_BITS ),
@@ -53,48 +52,47 @@ poly_mod_mult #(
   .COEF_BITS       ( COEF_BITS       ),
   .MODULUS         ( MODULUS         ),
   .REDUCTION_BITS  ( REDUCTION_BITS  )
-  
+
 )
 poly_mod_mult_i (
-  .i_clk   ( clk     ),
-  .i_rst   ( rst     ),
-  .i_val   ( start   ),
-  .i_dat_a ( i_dat_a ),
-  .i_dat_b ( i_dat_b ),
-  .o_dat   ( o_dat   ),
-  .o_val   ( o_val   )
+  .i_clk ( clk     ),
+  .i_rst ( rst     ),
+  .i_val ( start   ),
+  .i_dat ( i_dat_a ),
+  .o_dat ( o_dat   ),
+  .o_val ( o_val   )
 );
 
 task test_0();
 begin
   logic [1024-1:0] out, out_exp;
   $display("Running test_0...");
-  
+
   in_a = 2;
   i_dat_a = int_to_poly(in_a);
   for (int i = 0; i < 1000; i++) begin: LOOP_TEST
-  
+
     out_exp = (in_a**2) % MODULUS;
-    
+
     @(negedge clk);
     start = 1;
     @(negedge clk);
     start = 0;
     @(negedge clk);
-  
+
     while(!o_val) @(negedge clk);
     out = poly_to_int(o_dat);
-    
+
     // We might be off by several modulus - need to add wrapper TODO
     while (out > MODULUS) out -= MODULUS;
-    
+
     if(out != out_exp) begin
       $display("INFO: Input: %0d (0x%0x), Output: %0d (0x%0x), Expected: %0d (0x%0x)", in_a, in_a, out, out, out_exp, out_exp);
       $fatal(1, "ERROR - Output did not match");
     end else begin
       $display("INFO: Loop %0d (input %0d) OK", i, in_a);
     end
-    
+
     // Next inputs
     i_dat_a = o_dat;
     in_a = out;
@@ -119,7 +117,6 @@ endfunction
 initial begin
   start = 0;
   i_dat_a = 0;
-  i_dat_b = 0;
 
   #(40*CLK_PERIOD);
 
