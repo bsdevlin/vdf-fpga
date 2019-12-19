09af4bf61308135c285d32bb834da1daef19b2cb
bsdevlin <bsdevlin@LAPTOP-AL12KDU7>
2019-12-18 23:43:06 -0500

    _

--- redun_mont/src/tb/redun_mont_tb.sv
+++ redun_mont/src/tb/redun_mont_tb.sv
@@ -21,7 +21,7 @@ import redun_mont_pkg::*;
 import common_pkg::*;
 
 localparam CLK_PERIOD = 100;
-localparam NUM_ITERATIONS = 10;
+localparam NUM_ITERATIONS = 100000;
 
 logic clk, rst;
 redun0_t in, out;
@@ -30,7 +30,7 @@ logic in_val, out_val, overflow;
 
 initial begin
   rst = 0;
-  repeat(2) #(20*CLK_PERIOD) rst = ~rst;
+  repeat(2) #(10*CLK_PERIOD) rst = ~rst;
 end
 
 initial begin
@@ -52,40 +52,48 @@ redun_mont redun_mont (
 
 initial begin
   fe_t a, a_, res, exp;
+  int i;
   in_val = 0;
   in = to_redun(0);
-  #(40*CLK_PERIOD);
   
+  repeat (20) @(posedge clk);
+
   a = 2; // Our starting value
-  
   a_ = to_mont(a);
+
+  a_ = 'ha434820f4ac1a2d2182928233b96c22874e05d476afbbaff4d5242a58bc4d6f1dc9709c344ad39d3cf27c15d6b778b2f77df59890ec092b953ccbaade4c57ecccca6a6ee77cb82114d8f042461356f33786c7a48aa8bf577a95a1dc025ddff50888f2a984f1019c7348b64cf93a985a4e5cb28a0e3f369f968d6362e0db575e8;
+  a = from_mont(a_);
+  
   in = to_redun(a_);
 
-  @(posedge clk);
+  @(negedge clk);
   in_val = 1;
-  @(posedge clk);
+  @(negedge clk);
   in_val = 0;
   in = to_redun(0);
 
-  for (int i = 0; i < NUM_ITERATIONS; i++) begin
+  for (i = 0; i < NUM_ITERATIONS; i++) begin
     while (out_val == 0) @(posedge clk);
     $display("#%0d Expected, Got:\n0x%0x\n0x%0x", i, fe_mul_mont(a_, a_), from_redun(out));
-    assert (from_redun(out) == fe_mul_mont(a_, a_)) else $fatal("ERROR -  wrong");
+    assert (from_redun(out) == fe_mul_mont(a_, a_)) else begin
+      $display("ERROR -  wrong");
+      break;
+    end
     a_ = fe_mul_mont(a_, a_);
     @(posedge clk);
   end
-  
+
   res = from_mont(from_redun(out));
-  exp = mod_sq(a,  NUM_ITERATIONS);
-  
+  exp = mod_sq(a,  i);
+
   $display("Final result was:");
   $display("0x%0x", res);
-  
-  if (res != exp) 
+
+  if (res != exp)
     $fatal("ERROR - final result was wrong, expected:\n0x%0x", exp);
   else
     $display("Result was correct");
-  
+
   repeat(2) @(posedge clk);
   $finish();
 end
