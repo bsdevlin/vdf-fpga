a227e5c92c5e4adb08270a071fb9caf7edfe2d59
Ben Devlin <bsdevlin@gmail.com>
2020-01-03 05:52:01 -0500

    updates for sd

--- msu/rtl/sdaccel/tcl/package_kernel.tcl
+++ msu/rtl/sdaccel/tcl/package_kernel.tcl
@@ -1,32 +1,32 @@
 # /*******************************************************************************
 # Copyright (c) 2018, Xilinx, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
-# 
-# 
+#
+#
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 # this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
-# 
-# 
+#
+#
 # 3. Neither the name of the copyright holder nor the names of its contributors
 # may be used to endorse or promote products derived from this software
 # without specific prior written permission.
-# 
-# 
+#
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,THE IMPLIED 
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
-# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
@@ -37,21 +37,26 @@ set msu_path         $root_path/msu/rtl
 set sdaccel_path     $root_path/msu/rtl/sdaccel
 set primitives_path  $root_path/primitives/rtl
 set modsqr_path      $root_path/modular_square/rtl
+set mont_path        $root_path/redun_mont
 
 set path_to_packaged       "./packaged_kernel_${suffix}"
 set path_to_tmp_project    "./tmp_kernel_pack_${suffix}"
 
-create_project -force kernel_pack $path_to_tmp_project 
+create_project -force kernel_pack $path_to_tmp_project
 
 add_files -norecurse [glob $msu_path/msu.sv]
 add_files -norecurse [glob $msu_path/modular_square_wrapper.sv]
 add_files -norecurse [glob msuconfig.vh]
 add_files -norecurse [glob mem/*.dat]
 add_files -norecurse [glob mem/reduction_lut.sv]
-add_files -norecurse [glob $sdaccel_path/*.sv] 
+add_files -norecurse [glob $sdaccel_path/*.sv]
 add_files -norecurse [glob $sdaccel_path/*.v]
 add_files -norecurse [glob $primitives_path/*.sv]
 add_files -norecurse [glob $modsqr_path/*.sv]
+add_files -norecurse [glob $modsqr_path/*.sv]
+add_files -norecurse [glob $mont_path/src/rtl/*.sv]
+add_files -norecurse [glob $mont_path/ip/clk_wiz/clk_wiz_0.xci]
+add_files -norecurse [glob $mont_path/ip/fifo_generator_16/fifo_generator_16.xci]
 
 set_property top ${krnl_name} [current_fileset]
 
