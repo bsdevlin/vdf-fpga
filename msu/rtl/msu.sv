60e912c2427555862ddabcebefa098cd6affd7b7
Ben Devlin <bsdevlin@gmail.com>
2020-01-19 15:16:05 -0500

    update msu to include deadlock timer to make sure if we hang we still return something

--- msu/rtl/msu.sv
+++ msu/rtl/msu.sv
@@ -87,7 +87,7 @@ module msu
    logic                         sq_start;
    logic                         sq_finished;
 
-   logic                         final_iteration;
+   logic                         final_iteration, final_iteration_r;
 
    // AXI data storage
    logic [AXI_IN_BITS-1:0]       axi_in;
@@ -96,6 +96,7 @@ module msu
    logic                         axi_in_shift;
 
    logic locked;
+   logic [9:0] deadlock_timer;
 
    genvar                        gi;
 
@@ -103,6 +104,7 @@ module msu
    logic        reset_1d;
    always @(posedge clk) begin
       reset_1d <= reset;
+      final_iteration_r <= final_iteration;
    end
 
 
@@ -140,7 +142,7 @@ module msu
              next_state       = STATE_COMPUTE;
 
            STATE_COMPUTE:
-             if(t_current == t_final) begin
+             if(final_iteration_r) begin
                 next_state    = STATE_PREPARE_SEND;
              end else begin
                 next_state    = STATE_COMPUTE;
@@ -185,7 +187,7 @@ module msu
          t_current            <= t_current + 1;
       end
    end
-   assign final_iteration = sq_finished && (t_current == t_final-1);
+   assign final_iteration = (sq_finished && (t_current == t_final-1)) || &deadlock_timer;
 
    assign sq_start                  = state == STATE_START;
    assign s_axis_xfer_size_in_bytes = (AXI_IN_COUNT*AXI_BYTES_PER_TXN);
@@ -207,6 +209,16 @@ module msu
       .o_locked ( locked      )
     );
 
+    // This is to make sure we don't deadlock
+    always_ff @(posedge clk) begin
+      if (reset_1d) begin
+        deadlock_timer <= 0;
+      end else begin
+        deadlock_timer <= (sq_finished || state != STATE_COMPUTE) ? 0 : deadlock_timer + 1;
+      end
+    end
+
+
     // Convert our data type
     always_comb begin
       sq_in_int = to_redun(0);
@@ -225,7 +237,7 @@ module msu
    always @(posedge clk) begin
       if(final_iteration) begin
          axi_out_count                 <= 0;
-         axi_out[T_LEN-1:0]            <= t_current;
+         axi_out[T_LEN-1:0]            <= &deadlock_timer ? {T_LEN{1'b1}} : t_current;
          axi_out[AXI_OUT_BITS-1:T_LEN] <= sq_out;
       end else if(state == STATE_SEND && m_axis_tready) begin
          axi_out                       <= { {AXI_LEN{1'b0}},
